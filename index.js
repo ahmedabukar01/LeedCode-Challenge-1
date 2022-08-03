e990b040af0371a8f8a078b63fde34b161d202c3
unknown <ahmedlearn01@gmail.com>
2022-08-03 11:06:33 +0300

    wrong answer

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 
+        // JAVASCRIPT SOLVING IN ONE OF TWO WAYS
+
+    // WAY ONE >
+
+// var twoSum = function(nums, target) {
+//     let result = []
+//      nums.map(num=>{
+//         console.log('1')
+//         nums.forEach((fNum, index)=>{
+//             // if(nums.indexOf(num) == nums.indexOf(fNum))
+//             // return;
+
+
+//             if(num + nums[index] === target){
+//                     result.push(num);
+//                     result.push(fNum)
+//                     target = 0;
+//             }
+//         })
+//     })
+
+//     return [nums.indexOf(result[0]), nums.indexOf(result[1])]
+    
+// };
+
 var twoSum = function(nums, target) {
     let result = []
-     nums.map(num=>{
-        console.log('1')
-        nums.forEach((fNum, index)=>{
-            // if(nums.indexOf(num) == nums.indexOf(fNum))
-            // return;
+     nums.forEach((num,index)=>{
+
+        for(i= 0; i<nums.length; i++){
+
+                if(index === i){
+                    console.log(index, i)
+                }
+                 if(index === i)
+                 continue
 
 
-            if(num + nums[index] === target){
-                    result.push(num);
-                    result.push(fNum)
+                 if(num + nums[i] === target){
+                     result.push(nums[i])
+                     result.push(num);
                     target = 0;
             }
-        })
+        }
+    
     })
 
     return [nums.indexOf(result[0]), nums.indexOf(result[1])]
     
 };
 
-console.log(twoSum([3,3,3], 6));
+console.log(twoSum([3,3], 6));
